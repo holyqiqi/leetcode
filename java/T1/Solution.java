3ff2140b8817822a60115e1661fceb90288cd81c
zhumengqi <zhumengqi@daba.cn>
2016-11-03 14:57:11 +0800

    [1] 简化1的语句

--- java/T1/Solution.java
+++ java/T1/Solution.java
@@ -1,28 +1,22 @@
+/**
+ * 将数组中的数放入HashTable，键为索引，值为值
+ */
+
 package T1;
 
 import java.util.Hashtable;
-
-/**
- * 将数组中的数放入HashTable，键为索引，值为值
- * TODO 1.执行时间超时。2.数组中可以有重复的数
- */
-
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
         Hashtable<Integer, Integer> hashNums = new Hashtable<>();
         int i = 0;
-        int[] result = {0,0};
         for (int num : nums) {
             int sub = target - nums[i];
             if (hashNums.get(sub) != null && hashNums.get(sub) != i) {
-                result[1] = i;
-                result[0] = hashNums.get(sub);
-                return result;
+                return new int[]{hashNums.get(sub), i};
             } else {
                 hashNums.put(num, i++);
-                continue;
             }
         }
-        return result;
+        return new int[]{0, 0,};
     }
 }
