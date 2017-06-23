d212688c59001a2e315105c5c774b613c890e5ed
zhumengqi <mickey.zh.cn@gmail.com>
2017-06-23 18:00:17 +0800

    第2题解决

--- java/t2/Solution.java
+++ java/t2/Solution.java
@@ -2,22 +2,50 @@ package t2;
 
 /**
  * Created by zhumengqi on 2017-6-23.
+ * 第二题
+ * 阻塞点: 1.创建两个链表时写错了l2为L1,导致NullPointerExcep.
+ * 2.生成结果时,如何连接每个result节点: 头节点不动,声明一个指针节点顺序移动,修改next值
  */
 
 //Definition for singly-linked list.
 class ListNode {
-     int val;
-     ListNode next;
-     ListNode(int x) { val = x; }
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
 }
+
 public class Solution {
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode ln = new ListNode(0);
-        ln.next = l1;
+        ListNode sum = new ListNode(0);
+        ListNode before = sum;
+        int flag = 0;
         do {
-            ln = ln.next;
-            System.out.println("当前数字为: " + ln.val);
-        }while(!ln.equals(null));
-        return ln;
+            if (l1 == null) {
+                l1 = new ListNode(0);
+            }
+            if (l2 == null) {
+                l2 = new ListNode(0);
+            }
+            ListNode nodeSum = new ListNode(0);
+            nodeSum.val = l1.val + l2.val + flag;
+            flag=0;
+            if (nodeSum.val >= 10) {
+                nodeSum.val = nodeSum.val - 10;
+                flag = 1;
+            }
+            before.next = nodeSum;
+            before = nodeSum;
+            l1 = l1.next;
+            l2 = l2.next;
+
+        } while (l1 != null || l2 != null);
+        if (flag == 1) {
+            before.next = new ListNode(flag);
+        }
+        sum = sum.next;
+        return sum;
     }
 }
--- test/t2/SolutionTest.java
+++ test/t2/SolutionTest.java
@@ -13,42 +13,34 @@ import static t2.Solution.addTwoNumbers;
 * @since <pre>六月 23, 2017</pre> 
 * @version 1.0 
 */ 
-public class SolutionTest { 
+public class SolutionTest {
 
-@Before
-public void before() throws Exception { 
-} 
+    @Before
+    public void before() throws Exception {
+    }
 
-@After
-public void after() throws Exception { 
-} 
+    @After
+    public void after() throws Exception {
+    }
 
-/** 
-* 
-* Method: addTwoNumbers(ListNode l1, ListNode l2) 
-* 
-*/ 
-@Test
-public void testAddTwoNumbers() throws Exception {
-    ListNode l1 = new ListNode(2);
-    l1.next = new ListNode(4);
-    l1.next.next = new ListNode(3);
-    l1.next.next.next = l1;
-//    ListNode l1t2 = new ListNode(4);
-//    ListNode l1t3 = new ListNode(3);
-//    l1t3.next=null;
-//    l1t2.next = l1t3;
-//    l1.next = l1t2;
-    ListNode l2 = new ListNode(2);
-    ListNode l2t1 = new ListNode(4);
-    ListNode l2t2 = new ListNode(3);
-    l1.next = l2t1;
-    l2t1.next = l2t2;
-    l2t2.next=null;
+    /**
+     * Method: addTwoNumbers(ListNode l1, ListNode l2)
+     */
+    @Test
+    public void testAddTwoNumbers() throws Exception {
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
 
-    ListNode ln = addTwoNumbers(l1,l2);
-    System.out.println();
-} 
+//        ListNode l1 = new ListNode(1);
+//        ListNode l2 = new ListNode(9);
+//        l2.next = new ListNode(9);
 
-
+//        ListNode l2 = new ListNode(5);
+        ListNode ln = addTwoNumbers(l1, l2);
+        System.out.println(ln.val + ", " + ln.next.val + ", " + ln.next.next.val);
+    }
 } 
