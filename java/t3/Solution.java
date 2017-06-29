f697e1c2c1a6c4b3f6c1636c9a82d0515bda7919
zhumengqi <mickey.zh.cn@gmail.com>
2017-06-29 17:36:47 +0800

    第3题解决: shit.

--- java/t3/Solution.java
+++ java/t3/Solution.java
@@ -1,19 +1,42 @@
 package t3;
-
-/**
- * Created by mike on 16-10-31.
- * 输入pwwkew,返回wke 最长连续子串,但不能包含重复字符.
- * 思路:
+/*
+  Created by mike on 16-10-31.
+  输入pwwkew,返回wke 最长连续子串,但不能包含重复字符.
+  思路:
  */
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Set;
+
 public class Solution {
     public static int lengthOfLongestSubstring(String s) {
-        int max = 0;
-        return max+1;
-/*        Hashtable<Character, Integer> subTable = new Hashtable<>();
-        for (int i=0;i<s.length();i++) {
-            int curMax = 0;
-            int lastChar = subTable.get(s.charAt(i));
+        Hashtable<Character, Integer> subTable = new Hashtable<>();
+        int finalMax = 0;
+        for (int i = 0; i < s.length(); i++) {
+            boolean existsFlag = subTable.get(s.charAt(i)) != null;
+            if (existsFlag) {
+                finalMax = finalMax > subTable.size() ? finalMax : subTable.size();
+                int oldPos = subTable.get(s.charAt(i));
+                Set<Character> keySet = subTable.keySet();
+                List<Character> willRemove = new ArrayList<>();
+                for (char key : keySet) {
+                    int curPos = subTable.get(key);
+                    if (curPos < oldPos) {
+                        willRemove.add(key);
+                    }
+                }
+                for (char key : willRemove) {
+                    subTable.remove(key);
+                }
+                willRemove.clear();
+                subTable.put(s.charAt(i), i);
+            } else {
+                subTable.put(s.charAt(i), i);
+            }
+            finalMax = finalMax > subTable.size() ? finalMax : subTable.size();
         }
-        return max+1;*/
+        return finalMax;
     }
 }
